8a8e61734dcb5b0f7675b07c13edb4f4f669fbb1
felixacdc <pfmata01@gmail.com>
2017-01-25 23:00:36 -0600

    [modify] http verbs :imp:

--- 02/index.js
+++ 02/index.js
@@ -4,7 +4,27 @@ var express = require('express'),
 
 var fs = require('fs'),
     _ = require('lodash'),
-    engines = require('consolidate');
+    engines = require('consolidate'),
+    bodyParser = require('body-parser');
+
+function getUserFilePath (username) {
+  return path.join(__dirname, 'users', username) + '.json';
+}
+
+function getUser (username) {
+  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}));
+  user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+  _.keys(user.location).forEach(function (key) {
+    user.location[key] = _.startCase(user.location[key]);
+  });
+  return user;
+}
+
+function saveUser (username, data) {
+  var fp = getUserFilePath(username);
+  fs.unlinkSync(fp);
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'});
+}
 
 var users = [];
 
@@ -22,6 +42,7 @@ app.set('views', './views');
 app.set('view engine', 'hbs');
 
 app.use('/profilepics', express.static('images'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index', {users});
@@ -46,19 +67,14 @@ app.get('/:username', (req, res) => {
     });
 });
 
+app.put('/:username', (req, res) => {
+    var username = req.params.username;
+    var user = getUser(username);
+    user.location = req.body;
+    saveUser(username, user);
+    res.end();
+});
+
 var server = app.listen(3000, () => {
     console.log('Server running at http://localhost:' + server.address().port)
 });
-
-function getUser (username) {
-  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}));
-  user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
-  _.keys(user.location).forEach(function (key) {
-    user.location[key] = _.startCase(user.location[key]);
-  });
-  return user;
-}
-
-function getUserFilePath (username) {
-  return path.join(__dirname, 'users', username) + '.json';
-}
