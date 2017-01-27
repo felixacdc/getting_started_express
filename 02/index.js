e273e9f78355e548e0b23e635d8a7ec67f1da8e7
felixacdc <pfmata01@gmail.com>
2017-01-26 22:22:58 -0600

    [add] routing code organization :seat:

--- 02/index.js
+++ 02/index.js
@@ -5,38 +5,8 @@ var express = require('express'),
 var fs = require('fs'),
     _ = require('lodash'),
     engines = require('consolidate'),
-    bodyParser = require('body-parser');
-
-function getUserFilePath (username) {
-  return path.join(__dirname, 'users', username) + '.json';
-}
-
-function getUser (username) {
-  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}));
-  user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
-  _.keys(user.location).forEach(function (key) {
-    user.location[key] = _.startCase(user.location[key]);
-  });
-  return user;
-}
-
-function saveUser (username, data) {
-  var fp = getUserFilePath(username);
-  fs.unlinkSync(fp);
-  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'});
-}
-
-function verifyUser(req, res, next) {
-    var fp = getUserFilePath(req.params.username);
-
-    fs.exists(fp, (yes) => {
-        if(yes) {
-            next();
-        } else {
-            res.redirect('/error/' + req.params.username);
-        }
-    });
-}
+    bodyParser = require('body-parser'),
+    helpers = require('./helpers');
 
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
@@ -77,41 +47,41 @@ app.get('*.json', (req, res) => {
 
 app.get('/data/:username', (req, res) => {
     var username = req.params.username;
-    var user = getUser(username);
+    var user = helpers.getUser(username);
     res.json(user);
 });
 
-app.all('/:username', (req, res, next) => {
-    console.log(req.method, 'for', req.params.username);
-    next();
-});
-
-app.get('/:username', verifyUser, (req, res) => {
-    var username = req.params.username;
-    var user = getUser(username);
-    res.render('user', {
-        username,
-        address: user.location
+app.route('/:username')
+    .all((req, res, next) => {
+        console.log(req.method, 'for', req.params.username);
+        next();
+    })
+    .get(helpers.verifyUser, (req, res) => {
+        var username = req.params.username;
+        var user = helpers.getUser(username);
+        res.render('user', {
+            username,
+            address: user.location
+        })
+    })
+    .put((req, res) => {
+        var username = req.params.username;
+        var user = helpers.getUser(username);
+        user.location = req.body;
+        helpers.saveUser(username, user);
+        res.end();
+    })
+    .delete((req, res) => {
+        var fp = helpers.getUserFilePath(req.params.username);
+        fs.unlinkSync(fp);
+        res.sendStatus(200);
     });
-});
 
 app.get('/error/:username', (req, res) => {
     res.status(404).send('No user named ' + req.params.username + ' found');
 });
 
-app.put('/:username', (req, res) => {
-    var username = req.params.username;
-    var user = getUser(username);
-    user.location = req.body;
-    saveUser(username, user);
-    res.end();
-});
 
-app.delete('/:username', (req, res) => {
-    var fp = getUserFilePath(req.params.username);
-    fs.unlinkSync(fp);
-    res.sendStatus(200);
-});
 
 var server = app.listen(3000, () => {
     console.log('Server running at http://localhost:' + server.address().port)
