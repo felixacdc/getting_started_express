a7e9d834a916280e38d16504576ac15f8f42bb58
felixacdc <pfmata01@gmail.com>
2017-01-28 22:13:08 -0600

    [modify] mongodb integration :bear:

--- 02/db.js
+++ 02/db.js
@@ -1,5 +1,33 @@
 var uri = 'mongodb://localhost:27017/test';
 
+var mongoose = require('mongoose');
+mongoose.connect(uri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (callback) => {
+    console.log('db connected');
+});
+
+var userSchema = mongoose.Schema({
+    username: String,
+    gender: String,
+    name: {
+        title: String,
+        first: String,
+        last: String,
+        full: String
+    },
+    location: {
+        street: String,
+        city: String,
+        state: String,
+        zip: Number
+    }
+});
+
+exports.User = mongoose.model('User', userSchema);
+
 // con mongo nativo
 /*var MongoClient = require('mongodb').MongoClient;
 
